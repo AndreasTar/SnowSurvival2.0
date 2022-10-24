8d329a4a892d5380b1b26a53a471dcae404479af
Andy Pandy <andytgamedev@gmail.com>
2022-10-24 13:00:43 +0300

    Added Movement Controller
    A very basic Movement Controller using Character Controller from Unity, with help from a Brackeys Video

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+
+    [SerializeField] CharacterController cC;
+    [SerializeField] Transform groundCheckPoint;
+
+    [SerializeField] float movementSpeed = 5f;
+    public float gravity = -9.81f;
+
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundMask;
+
+    [SerializeField] float turnSmoothTime = 0.1f;
+
+    float turnSmoothVelocity;
+    bool isGrounded;
+    Vector3 downVel = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        isGrounded = Physics.CheckSphere(groundCheckPoint.position, groundDistance, groundMask);
+
+        gravity = isGrounded ? 0 : -9.81f;
+        downVel = isGrounded ? Vector3.zero : downVel;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        }
+        downVel.y += gravity * Time.deltaTime * Time.deltaTime;
+        cC.Move(downVel);
+        cC.Move(direction * movementSpeed * Time.deltaTime);
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(groundCheckPoint.position, groundDistance);
+    }
+}
